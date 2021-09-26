276aaf6f20768115f6ebebab81a7b509f5092621
Igor Babkin <igba14@gmail.com>
2021-09-26 21:57:35 +0200

    Revert "refactor: folder structure"
    This reverts commit b5ecb96b708c95461b3df0e8a641b2c11eab7b04.

--- src/web/entry.tsx
+++ src/web/entry.tsx
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import './ui/main.scss';
 import { render } from 'react-dom';
 import React from 'react';
-import { Application } from './components/Application';
+import { Application } from './views/Application';
 import { LocatorContext } from 'react-clean-reactive-architecture';
 import { LocatorAdapter } from './core/LocatorAdapter';
 import { createLocator, EnvType } from './locator';
