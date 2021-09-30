c6938f31ed9b8e1ca64db331abdd5d60e8870b75
Igor Babkin <igba14@gmail.com>
2021-09-30 02:17:11 +0200

    refactor: redesign ui

--- application/useCases/todo/GetTodoList.ts
+++ application/useCases/todo/GetTodoList.ts
@@ -1,13 +1,13 @@
 import { IQuery } from 'clean-use-case';
-import { ITodo } from '../../domain/todo/ITodo';
-import { Observable } from 'rxjs';
+import { ITodoState } from '../../domain/todo/ITodo';
+import { map, Observable } from 'rxjs';
 import { inject } from '../../decorators';
 import { ITodoStore, ITodoStoreKey } from '../../domain/todo/ITodoStore';
 
-export class GetTodoList implements IQuery<ITodo[]> {
+export class GetTodoList implements IQuery<ITodoState[]> {
   constructor(@inject(ITodoStoreKey) private todoStore: ITodoStore) {}
 
-  create(): Observable<ITodo[]> {
-    return this.todoStore.getItems();
+  create(): Observable<ITodoState[]> {
+    return this.todoStore.getItems().pipe(map((todos) => todos.map((i) => i.getState())));
   }
 }
